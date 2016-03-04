1b8978b942740b895a76d46a085706f435d4ecc3
Birgit Fullerton <birgit.fullerton@gmail.com>
2016-03-04 21:15:11 +0100

    added sketches for grove dust sensor

---
+++ dust_sensor/grovetothingspeak/grovetothingspeak.ino
@@ -0,0 +1,284 @@
+/*
+Thingspeak
+Cange SSID and PASSWORD and ThingSpeakKEY
+*/
+
+#define SSID "***REMOVED***"
+#define PASSWORD "***REMOVED***"
+
+#define ThingSpeakKEY "***REMOVED***"
+
+#define LED_WLAN 13
+
+#define DEBUG true
+
+#include <SoftwareSerial.h>
+SoftwareSerial esp8266(11, 12); // RX, TX
+
+const byte thingPost[] PROGMEM = {
+  80, 79, 83, 84, 32, 42, 85, 82, 76, 42, 32, 72, 84, 84, 80, 47, 49, 46, 49, 10, 72, 111, 115, 116, 58, 32, 97, 112, 105, 46, 116, 104, 105, 110, 103, 115, 112, 101, 97, 107, 46, 99, 111, 109, 10, 67, 111, 110, 110, 101, 99, 116, 105, 111, 110, 58, 32, 99, 108, 111, 115, 101, 10, 67, 111, 110, 116, 101, 110, 116, 45, 84, 121, 112, 101, 58, 32, 97, 112, 112, 108, 105, 99, 97, 116, 105, 111, 110, 47, 120, 45, 119, 119, 119, 45, 102, 111, 114, 109, 45, 117, 114, 108, 101, 110, 99, 111, 100, 101, 100, 10, 67, 111, 110, 116, 101, 110, 116, 45, 76, 101, 110, 103, 116, 104, 58, 32, 42, 76, 69, 78, 42, 10, 10, 42, 65, 80, 80, 69, 78, 68, 42, 10
+};
+
+int pin = 8;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 30000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+char s[32];
+
+void setup() {
+  Serial.begin(19200);
+  esp8266.begin(19200);
+   
+  if (!espConfig()) serialDebug();
+  else digitalWrite(LED_WLAN, HIGH);
+  pinMode(8,INPUT);
+  starttime = millis();
+}
+
+void loop() {
+   duration = pulseIn(pin, LOW);
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+
+  if ((millis()-starttime) > sampletime_ms)
+  {
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    Serial.print(lowpulseoccupancy);
+    Serial.print(",");
+    Serial.print(ratio);
+    Serial.print(",");
+    Serial.println(concentration);
+    lowpulseoccupancy = 0;
+    String dataString = "";
+    dataString += dtostrf(concentration, 9, 4, s);
+    dataString += ",";
+    dataString += dtostrf(ratio, 9, 4, s); 
+    Serial.println(dataString);
+    if (sendThingPost(ThingSpeakKEY, dataString)) debug("Update Send");
+    starttime = millis();
+  }
+    
+}
+
+
+//-----------------------------------------ThingsSpeak Functions------------------------------------
+
+boolean sendThingPost(String key, String value)
+{
+  boolean succes = true;
+  String  Host = "api.thingspeak.com";
+  String msg = "field1=" + value;
+  succes &= sendCom("AT+CIPSTART=\"TCP\",\"" + Host + "\",80", "OK");
+
+  String postRequest = createThingPost("/update", key, msg);
+
+  if (sendCom("AT+CIPSEND=" + String(postRequest.length()), ">"))
+  {
+    esp8266.print(postRequest);
+    esp8266.find("SEND OK");
+    if (!esp8266.find("CLOSED")) succes &= sendCom("AT+CIPCLOSE", "OK");
+  }
+  else
+  {
+    succes = false;
+  }
+  return succes;
+}  
+
+String createThingPost(String url, String key, String msg)
+{
+  String xBuffer;
+
+  for (int i = 0; i <= sizeof(thingPost); i++)
+  {
+    char myChar = pgm_read_byte_near(thingPost + i);
+    xBuffer += myChar;
+  }
+
+  String append = "api_key=" + key + "&" + msg;
+  xBuffer.replace("*URL*", url);
+  xBuffer.replace("*LEN*", String( append.length()));
+  xBuffer.replace("*APPEND*", append);
+
+  return xBuffer;
+}
+
+String createThingGet(String url, String key)
+{
+  String xBuffer;
+
+  for (int i = 0; i <= sizeof(thingPost); i++)
+  {
+    char myChar = pgm_read_byte_near(thingPost + i);
+    xBuffer += myChar;
+  }
+
+  String append = "api_key=" + key;
+  xBuffer.replace("POST", "GET");
+  xBuffer.replace("*URL*", url);
+  xBuffer.replace("*LEN*", String( append.length()));
+  xBuffer.replace("*APPEND*", append);
+
+  return xBuffer;
+}
+
+String createThingGet(String url, String key, String msg)
+{
+  String xBuffer;
+
+  for (int i = 0; i <= sizeof(thingPost); i++)
+  {
+    char myChar = pgm_read_byte_near(thingPost + i);
+    xBuffer += myChar;
+  }
+
+  String append = "api_key=" + key + "&" + msg;
+
+  xBuffer.replace("POST", "GET");
+  xBuffer.replace("*URL*", url);
+  xBuffer.replace("*LEN*", String( append.length()));
+  xBuffer.replace("*APPEND*", append);
+
+  return xBuffer;
+}
+
+//-----------------------------------------Config ESP8266------------------------------------
+
+boolean espConfig()
+{
+  boolean succes = true;
+  esp8266.setTimeout(5000);
+  succes &= sendCom("AT+RST", "ready");
+  esp8266.setTimeout(1000);
+
+  if (configStation(SSID, PASSWORD)) {
+    succes &= true;
+    debug("WLAN Connected");
+    debug("My IP is:");
+    debug(sendCom("AT+CIFSR"));
+  }
+  else
+  {
+    succes &= false;
+  }
+  //shorter Timeout for faster wrong UPD-Comands handling
+  succes &= sendCom("AT+CIPMUX=0", "OK");
+  succes &= sendCom("AT+CIPMODE=0", "OK");
+
+  return succes;
+}
+
+boolean configTCPServer()
+{
+  boolean succes = true;
+
+  succes &= (sendCom("AT+CIPMUX=1", "OK"));
+  succes &= (sendCom("AT+CIPSERVER=1,80", "OK"));
+
+  return succes;
+
+}
+
+boolean configTCPClient()
+{
+  boolean succes = true;
+
+  succes &= (sendCom("AT+CIPMUX=0", "OK"));
+  //succes &= (sendCom("AT+CIPSERVER=1,80", "OK"));
+
+  return succes;
+
+}
+
+
+boolean configStation(String vSSID, String vPASSWORT)
+{
+  boolean succes = true;
+  succes &= (sendCom("AT+CWMODE=1", "OK"));
+  esp8266.setTimeout(20000);
+  succes &= (sendCom("AT+CWJAP=\"" + String(vSSID) + "\",\"" + String(vPASSWORT) + "\"", "OK"));
+  esp8266.setTimeout(1000);
+  return succes;
+}
+
+boolean configAP()
+{
+  boolean succes = true;
+
+  succes &= (sendCom("AT+CWMODE=2", "OK"));
+  succes &= (sendCom("AT+CWSAP=\"NanoESP\",\"\",5,0", "OK"));
+
+  return succes;
+}
+
+boolean configUDP()
+{
+  boolean succes = true;
+
+  succes &= (sendCom("AT+CIPMODE=0", "OK"));
+  succes &= (sendCom("AT+CIPMUX=0", "OK"));
+  succes &= sendCom("AT+CIPSTART=\"UDP\",\"192.168.255.255\",90,91,2", "OK"); //Importand Boradcast...Reconnect IP
+  return succes;
+}
+
+
+
+
+//-----------------------------------------------Controll ESP-----------------------------------------------------
+
+boolean sendUDP(String Msg)
+{
+  boolean succes = true;
+
+  succes &= sendCom("AT+CIPSEND=" + String(Msg.length() + 2), ">");    //+",\"192.168.4.2\",90", ">");
+  if (succes)
+  {
+    succes &= sendCom(Msg, "OK");
+  }
+  return succes;
+}
+
+
+boolean sendCom(String command, char respond[])
+{
+  esp8266.println(command);
+  if (esp8266.findUntil(respond, "ERROR"))
+  {
+    return true;
+  }
+  else
+  {
+    debug("ESP SEND ERROR: " + command);
+    return false;
+  }
+}
+
+String sendCom(String command)
+{
+  esp8266.println(command);
+  return esp8266.readString();
+}
+
+
+
+//-------------------------------------------------Debug Functions------------------------------------------------------
+void serialDebug() {
+  while (true)
+  {
+    if (esp8266.available())
+      Serial.write(esp8266.read());
+    if (Serial.available())
+      esp8266.write(Serial.read());
+  }
+}
+
+void debug(String Msg)
+{
+  if (DEBUG)
+  {
+    Serial.println(Msg);
+  }
+}
